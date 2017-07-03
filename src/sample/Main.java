ffac0ecc468b30b259b63d6108637e657e3ef631
nohtewolde <noh.tewolde@kysu.edu>
2017-07-03 15:28:47 -0400

    V4 Self contained installation package for windows (.exe) and icons included.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,8 +6,12 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.*;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+
+import java.awt.*;
+import java.awt.Image;
 
 public class Main extends Application {
 
@@ -15,6 +19,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Brain Scholar Desktop");
+        primaryStage.getIcons().add(new javafx.scene.image.Image(getClass().getResourceAsStream("BS_Icon.png")));
+        //primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("BS_Icon.png")));
         primaryStage.setScene(new Scene(root, 1080, 800));
         primaryStage.show();
 
